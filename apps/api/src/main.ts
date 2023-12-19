efd67bc53f8c953fc59a9419576413f1dd266cce
BlueOrang3 <matealtunashvili@gmail.com>
2023-12-19 19:21:16 +0400

    added backend graphiql and users feature

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,14 +7,25 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { NestFastifyApplication, FastifyAdapter } from "@nestjs/platform-fastify";
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.register(helmet,{
+    // should be turned on in production
+    contentSecurityPolicy: false,
+  });
+
+  app.enableCors()
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`🚀 Application playground is running on: http://localhost:${port}/graphiql`);
 }
 
 bootstrap();
